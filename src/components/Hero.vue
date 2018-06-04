096fdf622ad655f77964acc774ffd041a48f8039
Suzannah James <suzannahsmjames1@gmail.com>
2018-06-04 13:23:26 +0100

    set up vue project

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero">
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Yoga Classes</li>
+        <li>Meditation</li>
+        <li>Explore</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <h1>Yoga & Meditation Teacher</h1>
+    <div class="logo">
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
