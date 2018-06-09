97739374efb49e141735bb39293361c6d266d9bd
Samuel Gregory <samuelgregory@me.com>
2018-06-09 21:32:37 +0100

    add meditation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <Hero />
 
     <div v-for="component in this.data" v-bind:key="component.slice_type">
-      <about v-if="component.slice_type === 'about'" v-bind:title="component.data.title" v-bind:text="component.data.text" v-bind:image="component.data.image">hero</about>
-      <yoga v-if="component.slice_type === 'yoga'" v-bind:privateText="component.data.privateText" v-bind:lessons="component.data.lessons"/>
-      <testimonials v-if="component.slice_type === 'testimonials'" v-bind:title="title" v-bind:testimonials="testimonials" />
+      {{component.slice_type}}
+      <about v-if="component.slice_type === 'about'" v-bind:title="component.data.title" v-bind:text="component.data.text" v-bind:image="component.data.image" />
+      <yoga v-if="component.slice_type === 'yoga'" v-bind:privateText="component.data.privateText" v-bind:lessons="component.data.lessons" />
+      <testimonials v-if="component.slice_type === 'testimonials'" v-bind:title="component.data.title" v-bind:testimonials="component.data.testimonials" />
+      <meditation v-if="component.slice_type === 'meditation'" v-bind:title="component.data.title" v-bind:text="component.data.text" v-bind:image="component.data.image" v-bind:quote="component.data.quote" v-bind:quoteBy="component.data.quoteBy" />
     </div>
   </div>
 </template>
@@ -15,6 +17,7 @@
   import About from './components/About.vue';
   import Yoga from './components/Yoga.vue';
   import Testimonials from './components/Testimonials.vue';
+  import Meditation from './components/Meditation.vue';
 
   export default {
     name: 'app',
@@ -22,7 +25,8 @@
       Hero,
       About,
       Yoga,
-      Testimonials
+      Testimonials,
+      Meditation
     },
     data: () => ({
       data: null,
@@ -62,15 +66,32 @@
           }
         },
         {
-          "slice_type": 'testimonials',
-          "data": {
-            "title": "Testimonials",
-            "testimonials": [
+          slice_type: 'testimonials',
+          data: {
+            title: 'Testimonials',
+            testimonials: [
               {
-                "text": "This is some great text",
-                "name": "Me"
+                text: 'This is some great text',
+                name: 'Me'
+              },
+              {
+                text: 'Another quote',
+                name: 'Someone else'
               }
             ]
+          }
+        },
+        {
+          slice_type: 'meditation',
+          data: {
+            title: 'Meditation',
+            text: '<p>Some text</p><a href="#">Some link</a>',
+            image: {
+              url: '/url',
+              alt: 'alt text'
+            },
+            quote: 'This is some quote',
+            quoteBy: 'Me'
           }
         }
       ]
@@ -79,7 +100,5 @@
 </script>
 
 <style>
-#app {
 
-}
 </style>
