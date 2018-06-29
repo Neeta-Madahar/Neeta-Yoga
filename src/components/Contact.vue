6a40c3c0ced2be24175b6a09eaee1b9b9013cbc1
Sam Gregory <samuel@digital-detox.co.uk>
2018-06-29 21:47:00 +0100

    style button and add contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,10 @@
 
     <p>{{ text }}</p>
 
-    <button>Pop me a message</button>
+    <button @click="openPopup">
+      <i class="icon-namaste" aria-hidden="true"></i>
+      Pop me a message
+    </button>
   </div>
 </template>
 
@@ -14,6 +17,20 @@
     props: {
       title: String,
       text: String,
+    },
+    beforeCreate: function() {
+      window.addEventListener('DOMContentLoaded', () => {
+        this.popup = window.typeformEmbed.makePopup('https://neetamadahar.typeform.com/to/UOk2Z7', {
+          mode: 'popup',
+          hideHeaders: true,
+          hideFooter: true
+        });
+      });
+    },
+    methods: {
+      openPopup: function() {
+        this.popup.open();
+      }
     }
   }
 </script>
@@ -22,13 +39,26 @@
   @import "../assets/sass/mixins";
 
   button {
-    padding: 20px 10px;
+    padding: 20px;
     border: 2px solid;
+    margin: 15px 0;
     transition: background-color .3s ease;
     background-color: transparent;
 
     @include hover-focus {
       color: $color-white;
+      background-color: $color-pink;
+
+      .icon-namaste {
+        color: currentColor;
+      }
     }
+  }
+
+  .icon-namaste {
+    font-size: 2em;
+    vertical-align: middle;
+    margin-right: 10px;
+    color: $color-pink;
   }
 </style>
