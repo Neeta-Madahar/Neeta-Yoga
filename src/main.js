5f9e52ad454ee13cd7c71f450fd14c827e05d4c6
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2018-11-06 21:21:44 +0000

    Update GA tracking ID's

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ new Vue({
 }).$mount('#app');
 
 Vue.use(VueAnalytics, {
-  id: ['UA-128847986-1', 'UA-128847986-2'],
+  id: ['UA-128817330-1', 'UA-128817330-2'],
   debug: {
     sendHitTask: process.env.NODE_ENV === 'production'
   }
