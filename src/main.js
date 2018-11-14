363373979372085e5a0cced6217a82b67111e65a
Samuel Gregory <samuelgregory@me.com>
2018-11-14 21:17:20 +0000

    add ga properly

--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router'
 import VueAnalytics from 'vue-analytics';
 
 import App from './App.vue';
 import Preview from './Preview.vue';
 import './assets/sass/main.scss';
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
-const routes = {
-  '/': App,
-  '/preview': Preview
-};
+const router = new VueRouter({
+  mode: 'history',
+  router: [
+    { path: '/', component: App },
+    { path: '/preview', component: Preview },
+    { path: '/:uid', redirect: '/' },
+  ],
+});
 
 
 new Vue({
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      this.$ga.page(this.currentRoute);
-      return routes[this.currentRoute] || App;
-    }
-  },
-  render (h) { return h(this.ViewComponent) }
+  router,
+  render: h => h(App),
 }).$mount('#app');
+
 
 Vue.use(VueAnalytics, {
   id: ['UA-128817330-1', 'UA-128817330-2'],
+  router,
   debug: {
     sendHitTask: process.env.NODE_ENV === 'production'
   }
